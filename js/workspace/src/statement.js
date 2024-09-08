6f76fb589b21c0fe837f28a11f432d37a345da10
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-09 08:56:03 +0900

    js Extracting a Function volumeCreditsFor

--- js/workspace/src/statement.js
+++ js/workspace/src/statement.js
@@ -21,6 +21,13 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
+
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
