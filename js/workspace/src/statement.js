7d524b6ce1a6c1483b52f0d0064f5525e2337414
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-09 08:38:08 +0900

    js Removing play variables

--- js/workspace/src/statement.js
+++ js/workspace/src/statement.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
 
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -23,6 +23,10 @@ function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
 }
 
 function amountFor(aPerformance, play) {
