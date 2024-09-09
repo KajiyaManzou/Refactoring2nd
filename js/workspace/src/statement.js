989218b7679d92e7dd4873650f6d7b9e035eb3d7
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-09 10:51:35 +0900

    js Moving function volumeCreditsFor

--- js/workspace/src/statement.js
+++ js/workspace/src/statement.js
@@ -10,6 +10,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -19,7 +20,6 @@ function statement(invoice, plays) {
 
     function amountFor(aPerformance) {
         let result = 0;
-
         switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
@@ -37,7 +37,13 @@ function statement(invoice, plays) {
             default:
                 throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
+        return result;
+    }
 
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 }
@@ -64,7 +70,7 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -75,12 +81,5 @@ function renderPlainText(data, plays) {
                 style: "currency", currency: "USD",
                 minimumFractionDigits: 2
             }).format(aNumber / 100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 }
