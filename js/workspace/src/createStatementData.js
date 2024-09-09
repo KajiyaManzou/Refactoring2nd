55870646150d3700b79febe800c00dab525fdbea
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-09 12:05:11 +0900

    js Changing the function declaration PerformanceCalculator constractor

--- js/workspace/src/createStatementData.js
+++ js/workspace/src/createStatementData.js
@@ -9,9 +9,9 @@ function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
-        const calcurator = new PerformanceCalculator(aPerformance);
+        const calcurator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
+        result.play = calcurator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
         return result;
@@ -60,7 +60,8 @@ function createStatementData(invoice, plays) {
 }
 
 class PerformanceCalculator {
-    constructor(aPerformance) {
+    constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
+        this.play = aPlay;
     }
 }
