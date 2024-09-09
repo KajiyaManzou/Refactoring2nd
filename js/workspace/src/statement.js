10f3a3ba232c1dd575417780f37cc8f020ac0b11
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-09 09:26:16 +0900

    js Inlining variables

--- js/workspace/src/statement.js
+++ js/workspace/src/statement.js
@@ -8,18 +8,17 @@ function statement(invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    let volumeCredits = totalVolumeCredits();
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function totalVolumeCredits() {
-        let volumeCredits = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += volumeCreditsFor(perf);
         }
-        return volumeCredits;
+        return result;
     }
 
     function usd(aNumber) {
