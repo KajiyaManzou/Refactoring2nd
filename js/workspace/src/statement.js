15e180716bcba792a5833eaa84effe15558c4f05
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-09 10:56:47 +0900

    js Moving function totalVolumeCredits

--- js/workspace/src/statement.js
+++ js/workspace/src/statement.js
@@ -5,6 +5,7 @@ function statement(invoice, plays) {
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -55,6 +56,14 @@ function statement(invoice, plays) {
         }
         return result;
     }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -65,16 +74,8 @@ function renderPlainText(data, plays) {
     }
 
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
-    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
