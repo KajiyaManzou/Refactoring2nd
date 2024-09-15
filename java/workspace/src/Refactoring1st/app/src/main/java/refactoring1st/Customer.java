1ec0609486afca512fe2d883de7c5847ce295b7f
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-15 14:14:22 +0900

    java Substituting temporary variables with queries getTotalFrequentRenterPoints

--- java/workspace/src/Refactoring1st/app/src/main/java/refactoring1st/Customer.java
+++ java/workspace/src/Refactoring1st/app/src/main/java/refactoring1st/Customer.java
@@ -19,18 +19,16 @@ class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration<Rental> rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRentalPoints();
 
             result += "\t" + each.getMovie().getTitle() + "\t" +
                 String.valueOf(each.getCharge()) + "\n";
         }
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
 
@@ -40,6 +38,16 @@ class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRentalPoints();
         }
         return result;
     }
