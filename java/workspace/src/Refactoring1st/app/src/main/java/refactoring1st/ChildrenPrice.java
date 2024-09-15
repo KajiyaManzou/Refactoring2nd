55dc391bf71f948ba01a98ba6d6e800116fc87ac
KajiyaManzou <kajiya.coffee@gmail.com>
2024-09-15 15:10:56 +0900

    java Polymorphism for replacing condition statements getCharge

--- java/workspace/src/Refactoring1st/app/src/main/java/refactoring1st/ChildrenPrice.java
+++ java/workspace/src/Refactoring1st/app/src/main/java/refactoring1st/ChildrenPrice.java
@@ -1,7 +1,13 @@
 package refactoring1st;
 
 public class ChildrenPrice extends Price {
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.CHILDRENS;
+    }
+    public double getCharge(int dayRented) {
+        double result = 1.5;
+        if (dayRented > 3)
+            result += (dayRented - 3) * 1.5;
+        return result;
     }
 }
--- java/workspace/src/Refactoring1st/app/src/main/java/refactoring1st/NewReleasePrice.java
+++ java/workspace/src/Refactoring1st/app/src/main/java/refactoring1st/NewReleasePrice.java
@@ -1,7 +1,11 @@
 package refactoring1st;
 
 public class NewReleasePrice extends Price {
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.NEW_RELEASE;
+    }
+
+    public double getCharge(int dayRented) {
+        return dayRented * 3;
     }
 }
